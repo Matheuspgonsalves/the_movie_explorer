4e19d86da7ee9666c22a7162cc3bc9ed7c6dfbe3
Matheuspgonsalves <matheus.gonsalvespereira@gmail.com>
2025-04-27 22:51:21 -0300

    feat: criar service para busca de filme

---
+++ lib/app/data/services/movie_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:the_movie_explorer/app/core/constants/movie_api_constants.dart';
+import 'package:http/http.dart' as http;
+import '../models/movie_model.dart';
+
+class MovieApiService {
+  Future<List<MovieModel>> searchMovies(String query) async {
+    final url = Uri.parse(
+      '${MovieApiConstants.baseUrl}/search/movie?api_key=${MovieApiConstants.apiKey}&query=$query',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonBody = json.decode(response.body);
+
+      final List<dynamic> results = jsonBody['results'];
+
+      List<MovieModel> movies = results
+          .map((movieJson) => MovieModel.fromMap(movieJson))
+          .toList();
+
+      return movies;
+    } else {
+      throw Exception('Erro ao buscar filmes: ${response.statusCode}');
+    }
+  }
+}
